dfe3f94cb2c1abf2b89d935cddb1a8595f54665b
ericjyr <ericjyr@gmail.com>
2024-02-06 16:16:51 -0700

    Landing page created and styled

---
+++ src/components/Cards.js
@@ -0,0 +1,11 @@
+// src/Cards.js
+import React from 'react';
+
+const Cards = () => (
+  <div>
+    <h2>Cards Component</h2>
+    <p>This is the component for card swiping facts.</p>
+  </div>
+);
+
+export default Cards;
---
+++ src/components/CardsPage.js
@@ -0,0 +1,13 @@
+// src/CardsPage.js
+import React from 'react';
+import Cards from './Cards';
+import { Container, Row, Col } from "react-bootstrap";
+
+const CardsPage = () => (
+  <div>
+    <h2>Cards Page</h2>
+    <Cards />
+  </div>
+);
+
+export default CardsPage;
